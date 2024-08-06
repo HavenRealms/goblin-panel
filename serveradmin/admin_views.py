e0d4aa31142f13d3f9985f016ff7ef9ab3d0bf0c
FlopsiBunny <71574715+FlopsiBunny@users.noreply.github.com>
2024-08-06 22:22:13 +1200

    Add Core Dashboard Assets

--- serveradmin/admin_views.py
+++ serveradmin/admin_views.py
@@ -6,3 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 class AdminDashboardView(LoginRequiredMixin, TemplateView):
     template_name = "admin/dashboard.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Dashboard'
+        return context
