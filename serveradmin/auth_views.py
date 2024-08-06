59762b1ce0fe9b8abca3fa2404bc5c1aae17fdad
FlopsiBunny <71574715+FlopsiBunny@users.noreply.github.com>
2024-08-06 21:27:42 +1200

    Add Redirection for Already Logged in Users

--- serveradmin/auth_views.py
+++ serveradmin/auth_views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import TemplateView
 
 # Create your views here.
 class AuthLoginView(TemplateView):
     template_name = "auth/login.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse('admin-dashboard-home'))
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -12,6 +18,12 @@ class AuthLoginView(TemplateView):
 
 class AuthRegisterView(TemplateView):
     template_name = "auth/register.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse('admin-dashboard-home'))
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['page_title'] = 'Registration'
--- serveradmin/urls.py
+++ serveradmin/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .auth_views import *
+from .admin_views import *
 
 urlpatterns = [
-    path('auth/login/', AuthLoginView.as_view()),
-    path('auth/register/', AuthRegisterView.as_view()),
+    path('auth/login/', AuthLoginView.as_view(), name="auth-login"),
+    path('auth/register/', AuthRegisterView.as_view(), name="auth-register"),
+    path('admin/', AdminDashboardView.as_view(), name="admin-dashboard-home"),
 ]
