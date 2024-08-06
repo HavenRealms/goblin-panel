23c48efb94483cef81100b6fa9bdfd302eb09f38
FlopsiBunny <71574715+FlopsiBunny@users.noreply.github.com>
2024-08-06 21:20:54 +1200

    Add Authentication Templates

--- serveradmin/views.py
+++ serveradmin/views.py
@@ -5,5 +5,14 @@ from django.views.generic import TemplateView
 class AuthLoginView(TemplateView):
     template_name = "auth/login.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Admin Login'
+        return context
+
 class AuthRegisterView(TemplateView):
     template_name = "auth/register.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Registration'
+        return context
