6c8812654ebd98935e7968713e37ab5edc6c2fd3
FlopsiBunny <71574715+FlopsiBunny@users.noreply.github.com>
2024-08-06 21:44:15 +1200

    Update Login Template to Actually Login

--- serveradmin/auth_views.py
+++ serveradmin/auth_views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.urls import reverse
 from django.views.generic import TemplateView
 from contrib.auth.mixins import LoggedOutRequiredMixin
@@ -6,6 +7,19 @@ from contrib.auth.mixins import LoggedOutRequiredMixin
 # Create your views here.
 class AuthLoginView(LoggedOutRequiredMixin, TemplateView):
     template_name = "auth/login.html"
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse('admin-dashboard-home'))
+        else:
+            return render(request, self.template_name, {
+                'page_title': 'Admin Login',
+                'error': 'Invalid credentials'
+            })
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
