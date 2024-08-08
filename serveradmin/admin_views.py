6d21a4c4b615321629acf95f3ed7cd29c427eab6
FlopsiBunny <71574715+FlopsiBunny@users.noreply.github.com>
2024-08-08 12:49:00 +1200

    Added Node Creation

--- serveradmin/admin_views.py
+++ serveradmin/admin_views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView, View
+from django.contrib.auth.models import User
 from django.http import HttpResponseBadRequest
 from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.conf import settings
 from .models import *
+from json import dumps
 
 # Create your views here.
 class AdminDashboard404View(LoginRequiredMixin, TemplateView):
@@ -117,5 +119,45 @@ class AdminNodeCreateView(LoginRequiredMixin, TemplateView):
         context["page_title"] = "Create Node"
         context["version"] = settings.VERSION
         context["locations"] = Location.objects.all()
+        context["users"] = User.objects.all()
         context["user"] = self.request.user
         return context
+    def post(self, request, *args, **kwargs):
+        # Get the POST data
+        name = request.POST.get('serverName')
+        description = request.POST.get('serverDescription')
+        location = request.POST.get("serverLocation")
+        address = request.POST.get("serverAddress")
+        ssl = request.POST.get("serverSSL")
+        proxy = request.POST.get("serverProxy")
+        memory = request.POST.get("serverMemory")
+        memory_over = request.POST.get("serverMemoryOver")
+        disk = request.POST.get("serverDiskSpace")
+        disk_over = request.POST.get("serverDiskSpaceOver")
+        upload_max = request.POST.get("serverMaxUploadSize")
+        visible = request.POST.get("serverVisible")
+
+        # Check if required data is present
+        if not name or not location or not address or not ssl or not proxy or not memory or not memory_over or not disk or not disk_over or not upload_max or not visible:
+            return HttpResponseBadRequest("Missing required fields: " + dumps(request.POST))
+
+        if name == "" or location == "" or address == "" or memory == "" or memory_over == "" or disk == "" or disk_over == "" or upload_max == "":
+            redirect("admin-nodes")
+
+        # Create and save the new Node instance
+        location = Location.objects.get(pk=int(location))
+        Node.objects.create(
+            name=name,
+            description=description,
+            location=location,
+            fqdn=address,
+            memory=memory,
+            memory_overallocate=memory_over,
+            disk=disk,
+            disk_overallocate=disk_over,
+            max_upload_size=upload_max,
+            public=visible
+        )
+
+        # Redirect to the admin locations page
+        return redirect('admin-nodes')
