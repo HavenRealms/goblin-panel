290a83a058aecf6eb51c65388a11816a6b0e17d2
FlopsiBunny <71574715+FlopsiBunny@users.noreply.github.com>
2024-08-09 20:28:53 +1200

    Rename Nodes Creation Template

--- serveradmin/admin_views.py
+++ serveradmin/admin_views.py
@@ -112,7 +112,7 @@ class AdminNodesView(LoginRequiredMixin, TemplateView):
         return context
 
 class AdminNodeCreateView(LoginRequiredMixin, TemplateView):
-    template_name = "serveradmin/nodes-create.html"
+    template_name = "serveradmin/node-create.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
