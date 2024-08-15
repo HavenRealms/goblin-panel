119a0520eb0716beec2408711e47de45b19af4e2
FlopsiBunny <71574715+FlopsiBunny@users.noreply.github.com>
2024-08-15 23:45:35 +1200

    Update admin_views.py

--- serveradmin/admin_views.py
+++ serveradmin/admin_views.py
@@ -433,10 +433,10 @@ class AdminGemDetailView(LoginRequiredMixin, TemplateView):
     def post(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         if "update-gem" in request.POST:
-            print(request.POST)
             if "gem-file" in request.FILES:
                 gemFile = request.FILES.get("gem-file")
                 if gemFile != "":
+                    print(gemFile.name)
                     if not gemFile.name.endswith(".json"):
                         fs = FileSystemStorage()
                         filename = fs.save(gemFile.name, gemFile)
