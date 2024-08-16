c2a2411cfe47de92681988e8c97103e1323a68c4
Dan Alexander <admin@randamonium.co.nz>
2024-08-16 18:06:38 +1200

    Update models.py

--- serveradmin/models.py
+++ serveradmin/models.py
@@ -152,3 +152,6 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
     dark_mode = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
