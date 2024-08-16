c11db24108509d00113c037b0c49cae1b0ea44d6
Dan Alexander <admin@randamonium.co.nz>
2024-08-16 17:59:08 +1200

    Update admin.py

--- serveradmin/admin.py
+++ serveradmin/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from .models import *
 
 # Register your models here.
@@ -10,3 +12,17 @@ admin.site.register(Hoarde)
 admin.site.register(Gem)
 admin.site.register(Server)
 admin.site.register(Theme)
+admin.site.register(UserProfile)
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'User Profiles'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
