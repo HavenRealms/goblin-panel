df8ffd930cb8bbe5b3b581b79e951d7026231d53
Dan Alexander <admin@randamonium.co.nz>
2024-08-16 18:01:16 +1200

    Update admin.py

--- serveradmin/admin.py
+++ serveradmin/admin.py
@@ -3,17 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from .models import *
 
-# Register your models here.
-admin.site.register(Location)
-admin.site.register(Node)
-admin.site.register(Allocation)
-admin.site.register(Database)
-admin.site.register(Hoarde)
-admin.site.register(Gem)
-admin.site.register(Server)
-admin.site.register(Theme)
-admin.site.register(UserProfile)
-
+# Define an inline admin descriptor for the UserProfile model
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
@@ -26,3 +16,13 @@ class UserAdmin(BaseUserAdmin):
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+# Register the other models, but exclude UserProfile
+admin.site.register(Location)
+admin.site.register(Node)
+admin.site.register(Allocation)
+admin.site.register(Database)
+admin.site.register(Hoarde)
+admin.site.register(Gem)
+admin.site.register(Server)
+admin.site.register(Theme)
